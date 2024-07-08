6cc561a9c27d386b5fcf73fbc564387c408e47c9
Cem2204 <cem.cem.strix@gmail.com>
2024-07-08 16:27:40 +0300

    Starting...

---
+++ Structures.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Rectangle{
+    int length;
+    int breadth;
+    char x;
+};
+
+int main(){
+
+    struct Rectangle r;
+    r.length = 10;
+    r.breadth = 5;
+
+    cout<<r.length<<endl;
+    cout<<r.breadth<<endl;
+    cout<<sizeof(r)<<endl;
+
+    return 0;
+
+}
